17950d700bda7c846b06aad046cb40c4269570c3
Matthew Johnston <matthewjohnston4@outlook.com>
2015-10-20 16:27:44 +0000

    Removing some unwanted testing comments

--- bin/build.php
+++ bin/build.php
@@ -3,10 +3,10 @@ namespace HHVM\UserDocumentation;
 
 require(__DIR__.'/../vendor/autoload.php');
 function hhvm_to_yaml(): void {
-  //(new RawYAMLBuildStep())->buildAll();
-  //(new MergedYAMLBuildStep())->buildAll();
+  (new RawYAMLBuildStep())->buildAll();
+  (new MergedYAMLBuildStep())->buildAll();
   (new MergedMarkdownBuildStep())->buildAll();
-  //(new GuidesHTMLBuildStep())->buildAll();
+  (new GuidesHTMLBuildStep())->buildAll();
   (new APIHTMLBuildStep())->buildAll();
 }
 
